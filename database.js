e98c901c5193b0806d6886290727b747af34f434
Jubico-wol <jubico@wol.group>
2021-08-27 14:13:34 -0600

    commit

--- database.js
+++ database.js
@@ -1,33 +1,37 @@
 // const mysql = require('mysql');
 const mysql = require('mysql2');
 
-const mysqlConnection =  mysql.createConnection({
+const mysqlConnection =  mysql.createPool({
 
     // host: '127.0.0.1',
     // user: 'root',
     // password: '',
     // database : 'asistencias',
-    // port: 3306
+    // port: 3306,
+    // connectionLimit: 10,
+    // multipleStatements: true
     
 
     host: 'baj9a82sgal6klndqli9-mysql.services.clever-cloud.com',
     user: 'uszh9oepnyl7wmyb',
     password: 'GMcPy0J2Dd3absyNMROi',
     database : 'baj9a82sgal6klndqli9',
-    port: 3306   
+    port: 3306,
+    connectionLimit: 10,
+    multipleStatements: true
 
 
 });
 
 
-mysqlConnection.connect(function(err){
-    if(err){
-        console.log(err); 
-        return;
-    }else{
-        console.log('DataBase is Connected!');
-    }
-});
+// mysqlConnection.connect(function(err){
+//     if(err){
+//         console.log(err); 
+//         return;
+//     }else{
+//         console.log('DataBase is Connected!');
+//     }
+// });
 
 
 
